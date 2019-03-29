212461ee2a8b6316c98e903931e99e5598d60c9a
mhma <mhma@cookapps.com>
2019-03-29 15:26:49 +0900

    chore: setup2

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,20 @@
+// https://github.com/Microsoft/TypeScript/wiki/JSDoc-support-in-JavaScript
+
+/**
+ * cooktest cli
+ *
+ * This is Test module
+ */
+const cli = {
+  /**
+   * Cli Test Functions
+   * @param text text to display
+   */
+  test (text: string) {
+    console.log('cli test20: ' + text)
+  }
+}
+
+export = cli
+// declare var module: any
+// module.exports = cli
